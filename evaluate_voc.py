d69ead1ebdc886630e9131ae56036156c682a7dd
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2022-08-06 18:20:38 +0200

    Update transfer learning evaluation to load only model weights (to save disc space)

--- evaluate_voc.py
+++ evaluate_voc.py
@@ -1,39 +1,83 @@
+from typing import Dict
+from pathlib import Path
+from functools import partial
 import argparse
 
 from tqdm import tqdm
 
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 from torchmetrics import AveragePrecision
 
-from src.model import VocLinearEvalModule, ResnetMultiProj
+from src.model import ResnetMultiProj
+from src.model.voc_linear_eval_module import get_categories, encode_labels
 from src.utils import get_config, get_device
 
 
-def evaluate(args) -> None:
-    path_config_self = args.config_self
-    path_config_voc = args.config_voc
-    path_voc = args.path_voc
+def get_dl(config: Dict):
+    bs = config['batch_size']
+    n_workers = config['n_workers']
+    stage = 'val'
 
-    config_self = get_config(path_config_self)
-    config_voc = get_config(path_config_voc)
+    mean_val = (0.45704722, 0.43824774, 0.4061733)
+    std_val = (0.23908591, 0.23509644, 0.2397309)
+    stats = (mean_val, std_val)
+
+    trans = transforms.Compose([
+        transforms.Resize(330),
+        transforms.CenterCrop(300),
+        transforms.ToTensor(),
+        transforms.Normalize(*stats)
+    ])
+
+    path = Path('./data/VOCdevkit/VOC2007/ImageSets/Main')
+    if not path.exists():
+        # download dataset
+        datasets.VOCDetection(root='./data', year='2007', image_set=stage, download=True)
+
+    object_categories = get_categories(path)
+    target_transform = partial(encode_labels, object_categories=object_categories)
+
+    ds = datasets.VOCDetection(root='./data', year='2007', image_set=stage, download=False,
+                               transform=trans, target_transform=target_transform)
+    print(f'stage: {stage}, N: {len(ds)}')
+    train = stage == 'train'
+    dl = DataLoader(ds, batch_size=bs, shuffle=True, num_workers=n_workers, pin_memory=train)
+    return dl
+
+
+def evaluate(args) -> None:
+    # load config
+    path_config = args.config
+    config = get_config(path_config)
     device = get_device()
 
-    encoder = ResnetMultiProj(**config_self['encoder']).backbone
-    encoder.to(device)
-    encoder.eval()
-    # configure model for VOC evaluation
-    module = VocLinearEvalModule.load_from_checkpoint(encoder=encoder, config=config_voc, checkpoint_path=path_voc)
-    module = module.eval()
-    module = module.to(device)
+    # load checkpoint
+    path_ckpt = args.ckpt
+    ckpt = torch.load(path_ckpt, map_location=device)
 
-    dl_val = module.val_dataloader()
+    # load encoder
+    encoder = ResnetMultiProj(**config['encoder']).backbone
+    encoder.to(device)
+    encoder.load_state_dict(ckpt['encoder'])
+    encoder.eval()
+
+    # load classifier
+    classifier = nn.Linear(2048, config['dataset']['n_classes'])
+    classifier.to(device)
+    classifier.load_state_dict(ckpt['classifier'])
+    classifier.eval()
+
+    dl_val = get_dl(config)
 
     average_precision = AveragePrecision()
     for batch_x, batch_y in tqdm(dl_val):
         batch_x, batch_y = batch_x.to(device), batch_y.to(device)
 
         with torch.no_grad():
-            pred = module(batch_x)
+            pred = classifier(encoder(batch_x))
         curr_avg_prec = average_precision(pred, batch_y)
 
     print(f'Average precision: {average_precision.compute()}')
@@ -41,14 +85,7 @@ def evaluate(args) -> None:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_self',
-                        help='Path to the config file for the self-supervised model',
-                        type=str, required=True)
-    parser.add_argument('--config_voc',
-                        help='Path to the config file for the VOC model',
-                        type=str, required=True)
-    parser.add_argument('--path_voc',
-                        help='Path to the checkpoint file for the VOC model',
-                        type=str, required=True)
+    parser.add_argument('--config', help='Path to the config file', type=str, required=True)
+    parser.add_argument('--ckpt', help='Path to the checkpoint file', type=str, required=True)
     args = parser.parse_args()
     evaluate(args)
