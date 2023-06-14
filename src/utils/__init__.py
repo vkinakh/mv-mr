40f6a0909df77c9a1a3717f85852e8455eb3912b
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2023-06-14 16:48:55 +0200

    Update utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,5 @@
 from .summary_writer_with_sources import SummaryWriterWithSources
 from .utils import get_device, get_config, std_filter_torch, split_into_patches
 from .utils import infinite_loader, seed_everything
-from .tsne import run_tsne
 from .on_checkoint_hparams import OnCheckpointHparams
-from .model_utils import trunc_normal_
-from .model_utils import count_trainable_parameters, get_params_groups
-
+from .model_utils import count_trainable_parameters
