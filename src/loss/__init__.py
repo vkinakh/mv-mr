3ac4f56b73773548d6d865883d80363cdbb66c94
Vitaliy Kinakh <vitaliy.kinakh@unige.ch>
2023-06-14 15:58:27 +0200

    Remove unused code

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,3 +1,1 @@
 from .distance_correlation import DistanceCorrelation
-from .distill_kl import DistillKL
-
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,17 +1,10 @@
 from .resnet import ResnetMultiProj
 from .resnet import resnet8x4
-from .embedding_extractor import EmbeddingExtractor
-from .linear_evaluator import LinearEvaluator
 from .hog import HOGLayer
 
-from .warmup_cosine_schedule import WarmupCosineSchedule
-from .cosine_wd_schedule import CosineWDSchedule
-from .momentum_scheduler import MomentumScheduler
-from .cosine_warmup_scheduler import CosineWarmupScheduler
 from .cosine_annealing_warmup_restarts_scheduler import CosineAnnealingWarmupRestarts
 
 from .self_supervised_module import SelfSupervisedModule, OnlineFineTuner
 from .semi_supervised_module import SemiSupervisedModule
 from .voc_linear_eval_module import VocLinearEvalModule
 from .clip_self_supervised_module import CLIPSelfSupervisedModule
-from .clip_supervised_module import CLIPSupervisedModule
